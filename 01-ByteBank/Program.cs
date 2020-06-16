7d62f988b52f8478e5c41bb8c01f75d0503f85d8
Gabriel Brassaroto <gabriel.brassaroto@tcs.com>
2020-06-16 17:01:36 -0300

    Adicionar arquivos de projeto.

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDaGabriela = new ContaCorrente ();
+            contaDaGabriela.Titular = "Gabriela";
+
+            contaDaGabriela.Agencia = 15884;
+
+            contaDaGabriela.Numero = 12544;
+
+            contaDaGabriela.Saldo = 1000.0;
+            Console.WriteLine(contaDaGabriela.Titular);
+
+            Console.ReadLine();
+
+        }
+    }
+}
