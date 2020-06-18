b147c3ee99c825f766c2a735035a9ece100fc819
Gabriel Brassaroto <gabriel.brassaroto@tcs.com>
2020-06-18 15:27:57 -0300

    Funcao

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.Titular = "Burno";
+
+            Console.WriteLine(contaDoBruno.Saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+
+            Console.WriteLine(contaDoBruno.Saldo);
+            Console.WriteLine(resultadoSaque);
+
+
+            Console.ReadLine();
+        }
+    }
+}
